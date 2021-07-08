1635fa4d8f7e6c50ac00b2d067ca5feb76a68082
Jonata Santos <jonatafsa.js@gmail.com>
2021-07-07 21:23:56 -0300

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,13 +5,13 @@ import 'firebase/database'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDxdr16deQjM3MGRuU0xAEbB0zPPJm5qsw",
-  authDomain: "fearsocial-9b3d2.firebaseapp.com",
-  projectId: "fearsocial-9b3d2",
-  storageBucket: "fearsocial-9b3d2.appspot.com",
-  messagingSenderId: "177036929820",
-  appId: "1:177036929820:web:94b455595163e2c2da28d9",
-  measurementId: "G-2YP3FJMVRK"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 }
 firebase.initializeApp(firebaseConfig);
 
